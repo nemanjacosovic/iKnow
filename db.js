2584ddbe16a13833a66dfebbeb8b4056aba0408a
Nemanja Ćosović <ncosovic@gmail.com>
2025-05-03 19:18:17 +0200

    Add db connection

---
+++ db.js
@@ -0,0 +1,12 @@
+// db.js
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "iknowdbs",
+  password: "your_password", // Replace with your actual password
+  port: 5432,
+});
+
+module.exports = { pool };
