9812991f2beb1ae359a91853dbc028e42ad8ddda
Nemanja Ćosović <ncosovic@gmail.com>
2025-05-03 19:25:00 +0200

    Define Layout

---
+++ iknow-app/src/components/layout/Layout.js
@@ -0,0 +1,233 @@
+// src/components/layout/Layout.js
+import React, { useState } from "react";
+import { useHistory, Link } from "react-router-dom";
+import {
+  AppBar,
+  CssBaseline,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Divider,
+  Box,
+  Menu,
+  MenuItem,
+  makeStyles,
+} from "@material-ui/core";
+import {
+  Menu as MenuIcon,
+  Dashboard as DashboardIcon,
+  Category as CategoryIcon,
+  QuestionAnswer as QuestionIcon,
+  LibraryBooks as SetIcon,
+  People as PeopleIcon,
+  AccountCircle as AccountIcon,
+  ExitToApp as LogoutIcon,
+} from "@material-ui/icons";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Layout = ({ children, title }) => {
+  const classes = useStyles();
+  const history = useHistory();
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const user = JSON.parse(localStorage.getItem("user"));
+  const isAdmin = user && user.is_admin;
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    history.push("/");
+  };
+
+  const handleProfileClick = () => {
+    handleClose();
+    history.push("/profile");
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar}>
+        <Box p={2}>
+          <Typography variant="h6" noWrap>
+            Trivia Admin
+          </Typography>
+        </Box>
+      </div>
+      <Divider />
+      <List>
+        <ListItem button component={Link} to="/dashboard">
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button component={Link} to="/categories">
+          <ListItemIcon>
+            <CategoryIcon />
+          </ListItemIcon>
+          <ListItemText primary="Categories" />
+        </ListItem>
+        <ListItem button component={Link} to="/questions">
+          <ListItemIcon>
+            <QuestionIcon />
+          </ListItemIcon>
+          <ListItemText primary="Questions" />
+        </ListItem>
+        <ListItem button component={Link} to="/sets">
+          <ListItemIcon>
+            <SetIcon />
+          </ListItemIcon>
+          <ListItemText primary="Question Sets" />
+        </ListItem>
+        {isAdmin && (
+          <ListItem button component={Link} to="/users">
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Users" />
+          </ListItem>
+        )}
+      </List>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title} noWrap>
+            {title}
+          </Typography>
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="navigation">
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true,
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
